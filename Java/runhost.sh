4ec8e48eaa9366710756d40351b8eba5d3ce658d
iijimam <miijima@intersystems.com>
2021-05-14 06:01:43 +0000

    sourceコマンド使えないところ修正

--- Java/runhost.sh
+++ Java/runhost.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 source ./host-java-params.sh
 
-java -cp .:${PWD}/jar/intersystems-jdbc-3.2.0.jar:NativeAPI/Start.class NativeAPI.Start.java
+java -cp .:${PWD}/jar/intersystems-jdbc-3.2.0.jar:NativeAPI/Start.class NativeAPI.Start
 
 echo "----------------------"
 echo "** 処理終了しました **"
