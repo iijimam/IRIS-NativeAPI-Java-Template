7e0c31be703d34bdb46ef20a464b09a6b09a55fd
iijimam <miijima@intersystems.com>
2021-05-14 12:30:04 +0900

    host-linux版作成

--- Java/build-java-host.sh
+++ Java/build-java-host.sh
@@ -4,51 +4,13 @@ rm -rf jar
 # IRIS jarの取得 ./jarに格納
 # iris jdbc
 wget -P jar https://github.com/intersystems-community/iris-driver-distribution/raw/main/JDK18/intersystems-jdbc-3.2.0.jar
-# iris xep
-wget -P jar https://github.com/intersystems-community/iris-driver-distribution/raw/main/JDK18/intersystems-xep-3.2.0.jar
 
-# IRIS用jarをmavenローカルリポジトリへのインストール
-# iris jdbc
-echo "---------------------------------------------"
-echo "IRIS JDBC用JARをローカルリポジトリへインストール"
-echo "---------------------------------------------"
-mvn install:install-file \
--Dfile=${PWD}/jar/intersystems-jdbc-3.2.0.jar \
--DgroupId=com.intersystems \
--DartifactId=intersystems-jdbc \
--Dversion=3.2.0 \
--Dpackaging=jar \
--DgeneratePom=true
 
-# iris xep
+# コンパイル
 echo "---------------------------------------------"
-echo "IRIS xep用JARをローカルリポジトリへインストール"
+echo "　　コンパイルします"
 echo "---------------------------------------------"
-mvn install:install-file \
--Dfile=${PWD}/jar/intersystems-xep-3.2.0.jar \
--DgroupId=com.intersystems \
--DartifactId=intersystems-xep \
--Dversion=3.2.0 \
--Dpackaging=jar \
--DcreateChecksum=true
-
-# mvn clean
-mvn clean
-
-# mvn package 実行
-echo "---------------------------------------------"
-echo "  mvn package 実行"
-echo "---------------------------------------------"
-mvn package
-
-# jarをリネーム
-echo "---------------------------------------------"
-echo "　　JAR完成！"
-echo "---------------------------------------------"
-cp ${PWD}/target/gps-xep-template-1.0-jar-with-dependencies.jar ${PWD}/sample.jar
-
-# IRISコンテナ開始
-source ../params.sh
+java -cp ${PWD}/jar/intersystems-jdbc-3.2.0.jar ${PWD}/NativeAPI/Start.java
 
 # IRIS用コンテナが起動中かどうかの確認
 if [ "$(docker container ls -q -f name="$IRIS_CONTAINER")" ]; then
@@ -73,8 +35,8 @@ docker-compose -f ../docker-compose.yml ps
 
 # Java実行例
 echo -e "----------------------------------------------------------------------------\n"
-echo -e "＜実行例＞\n"
-echo -e "   ./runhost.sh GPXSamples/Sakurajima.gpx.xml\n" 
+echo -e "[Java実行例]n"
+echo -e "   ./runhost.sh" 
 echo " *** 実行前に接続先IRISのホスト名（またはIPアドレス）をご確認ください。 *** "
 echo "    接続先情報は、./host-java-params.sh に記載しています。適宜ご変更ください"
 echo -e "    java 実行前に　source ./host-java-params.sh　を実行してください\n"
--- Java/runhost.sh
+++ Java/runhost.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 source ./host-java-params.sh
 
-java -jar ${PWD}/sample.jar
-
-echo "\nStreetViewの向きを前向きにするための処理を実行します（IRIS内ルーチンを実行します）\n"
-docker-compose exec -f ../docker-compose.yml iris iris session iris enrich
+java -cp ${PWD}/jar/intersystems-jdbc-3.2.0.jar NativeAPI.Start.java
 
 echo "----------------------"
 echo "** 処理終了しました **"
