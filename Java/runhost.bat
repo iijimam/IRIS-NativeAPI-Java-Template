0e140219d768de6cc4f1beb4f0f906d4560d0332
iijimam <miijima@intersystems.com>
2021-05-14 06:38:21 +0000

    不要なメッセージ削除

--- Java/runhost.bat
+++ Java/runhost.bat
@@ -3,12 +3,6 @@ call host-java-params.bat
 
 java -cp .;%CD%\jar\intersystems-jdbc-3.2.0.jar;NativeAPI\Start.class NativeAPI.Start
 
-echo -----------------------------------------------------------------
-echo  [Prepating for StreetView.html] running enrich routine on IRIS
-echo -----------------------------------------------------------------
-
-docker-compose exec -f ../docker-compose.yml iris iris session iris enrich
-
 echo -----------------------
 echo  ** completed !! **
 echo -----------------------
